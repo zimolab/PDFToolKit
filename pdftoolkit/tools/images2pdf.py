62f0b2cf4a7a30c23302fc838b922d568a08ab08
zimolab <zimolab@aliyun.com>
2024-12-05 20:12:56 +0800

    Improve images2pdf tool - add more parameters - better error handling

--- pdftoolkit/tools/images2pdf.py
+++ pdftoolkit/tools/images2pdf.py
@@ -1,5 +1,5 @@
 """
-A group of tools to make a PDF file from a set of images.
+A tool used to make a PDF file from a set of images.
 
 @author: zimolab
 @date: 2024-12-03
@@ -8,17 +8,18 @@ A group of tools to make a PDF file from a set of images.
 import gc
 import time
 from dataclasses import dataclass
-from typing import Optional, Dict, List, Tuple, Sequence
+from typing import Optional, List, Tuple
 
 import pymupdf
-from py_multitasking import (
-    TaskContext,
-    with_process_pool_executor,
-    Scopes,
-    Scope,
-    with_thread_pool_executor,
-)
+from py_multitasking import TaskContext, with_process_pool_executor, Scopes, Scope
 from pyguiadapter.adapter import GUIAdapter
+from pyguiadapter.adapter.ucontext import is_function_cancelled
+from pyguiadapter.adapter.uoutput import uprint
+from pyguiadapter.adapter.uprogress import (
+    show_progressbar,
+    hide_progressbar,
+    update_progress,
+)
 from pyguiadapter.exceptions import ParameterError
 from pyguiadapter.extend_types import file_t, file_list_t
 from pyguiadapter.windows import DocumentBrowserConfig
@@ -30,27 +31,36 @@ from ..commons.ui.window import (
     DEFAULT_DOCUMENT_BROWSER_FONT_SIZE,
     DEFAULT_OUTPUT_BROWSER_HEIGHT,
 )
-from ..commons.validators.basic import ensure_non_empty_string
+from ..commons.utils import get_cup_count, close_safely
+from ..commons.validators.basic import ensure_non_empty_string, ensure_in_range
 from ..core.workloads_distributor import distribute_evenly
 
 
 @dataclass
-class ImageReadResult(object):
+class ImageData(object):
     page_index: Optional[int] = None
     page_bytes: Optional[bytes] = None
     page_rect: Optional[pymupdf.Rect] = None
     exception: Optional[Exception] = None
 
 
-def read_images(ctx: TaskContext, images: List[Tuple[int, str]]):
+def populate_image_data(ctx: TaskContext, images: List[Tuple[int, str]]):
     for i, image_filepath in images:
-        result = ImageReadResult(page_index=i)
+        result = ImageData(page_index=i)
         img_doc = None
         try:
+            # check if cancel event has been set
+            # if so, exit the loop to stop the task
+            if ctx.has_cancel_event() and ctx.is_cancel_event_set():
+                if img_doc:
+                    close_safely(img_doc)
+                break
+            # read image file and convert to PDF bytes
             img_doc = pymupdf.open(image_filepath)
-            img_rect = img_doc[0].rect
-            result.page_rect = img_rect
+            result.page_rect = img_doc[0].rect
             result.page_bytes = img_doc.convert_to_pdf()
+            # write result to output queue
+            # this will be read by the main process
             if ctx.has_output_queue():
                 ctx.write_output(result)
         except Exception as e:
@@ -59,85 +69,148 @@ def read_images(ctx: TaskContext, images: List[Tuple[int, str]]):
                 ctx.write_output(result)
         finally:
             if img_doc:
-                img_doc.close()
+                close_safely(img_doc)
 
 
-def images2pdf(images: file_list_t, output_file: file_t):
-    """
-    Convert a set of images to a PDF file.
+def make_blank_document(page_count: int) -> pymupdf.Document:
+    doc = pymupdf.Document()
+    for i in range(page_count):
+        doc.insert_page(i)
+    return doc
 
-    :param images:
-    :param output_file:
+
+def replace_page(
+    doc: pymupdf.Document, page_index: int, page_bytes: bytes, page_rect: pymupdf.Rect
+):
+    page_doc = pymupdf.open("pdf", page_bytes)
+    doc.delete_page(page_index)
+    page = doc.new_page(page_index, width=page_rect.width, height=page_rect.height)
+    page.show_pdf_page(page_rect, page_doc, 0)
+    close_safely(page_doc)
+
+
+def images2pdf(images: file_list_t, output_file: file_t, worker_count: int = 1):
+    """Convert a set of images to a PDF file.
+
+    :param images: A list of image file paths.
+    :param output_file: The output PDF file path.
+    :param worker_count: The number of worker processes. More workers may speed up the process, but may consume more memory.
     :return:
     """
     if not images:
         raise ParameterError("images", "no images provided")
     ensure_non_empty_string("output_file", output_file)
-    start_time = time.time_ns()
-    # image_list = [read_image(image_filepath) for image_filepath in images]
-    # image_list[0].save(output_file, "PDF", save_all=True, append_images=image_list[1:])
-
-    # doc = pymupdf.open()  # PDF with the pictures
-    #
-    # for i, file in enumerate(images):
-    #     img = pymupdf.open(file)
-    #     rect = img[0].rect
-    #     pdfbytes = img.convert_to_pdf()
-    #     img.close()
-    #     imgpdf = pymupdf.open("pdf", pdfbytes)
-    #     page = doc.new_page(width=rect.width, height=rect.height)
-    #     page.show_pdf_page(rect, imgpdf, 0)
-    # doc.save(output_file)
-    # doc.close()
-
-    images_items = [(i, filepath) for i, filepath in enumerate(images)]
-
-    # 创建一个空白的PDF文档
-    doc = pymupdf.Document()
-    # 创建空白文档
-    for i, file in enumerate(images):
-        doc.insert_page(i)
-
-    with with_process_pool_executor(max_workers=5) as manager:
-        scopes = Scopes.Null()
-        scopes.output_queue = Scope.Session
-        session = manager.map(
-            "task-", read_images, scopes, distribute_evenly(images_items, 5)
-        )
-
-        while not session.all_done:
-            images_read: Sequence[ImageReadResult] = (
-                session.context.read_output_until_empty()
-            )
-            if not images_read:
-                continue
-            for result in images_read:
-                if result.exception:
-                    print(
-                        f"Error reading image {result.page_index}: {result.exception}"
-                    )
-                pdf_bytes = result.page_bytes
-                pdf_rect = result.page_rect
-                pdf_index = result.page_index
-                if pdf_bytes and pdf_rect:
-                    img_pdf = pymupdf.open("pdf", pdf_bytes)
-                    doc.delete_page(pdf_index)
-                    page = doc.new_page(
-                        pdf_index, width=pdf_rect.width, height=pdf_rect.height
-                    )
-                    page.show_pdf_page(pdf_rect, img_pdf, 0)
-                    img_pdf.close()
-                    del img_pdf
-                    print(f"Image {pdf_index} added to PDF document.")
-    doc.save(output_file)
-    doc.close()
-
-    end_time = time.time_ns()
-    print(
-        f"PDF file saved to {output_file} in {(end_time - start_time) / 1e9:.2f} seconds."
+    ensure_in_range(
+        "worker_count",
+        worker_count,
+        minimum=1,
+        maximum=get_cup_count(),
+        include_minimum=True,
+        include_maximum=True,
     )
-    # del image_list
-    gc.collect()
+
+    start_time = time.time_ns()
+
+    doc = make_blank_document(len(images))
+    images_items = [(i, filepath) for i, filepath in enumerate(images)]
+    total = len(images_items)
+    finished = 0
+    try:
+        processing_error = None
+        with with_process_pool_executor(max_workers=worker_count) as manager:
+            scopes = Scopes.Null()
+            scopes.output_queue = Scope.Session
+            scopes.cancel_event = Scope.Session
+            workloads = distribute_evenly(images_items, worker_count)
+            session = manager.map("task-", populate_image_data, scopes, workloads)
+            show_progressbar(max_value=total, min_value=0)
+            while not session.all_done:
+                if is_function_cancelled():
+                    uprint("Canceling...")
+                    session.cancel_all(with_cancel_event_set=True)
+                    session.wait_for_all()
+                    # empty the output queue
+                    _ = session.context.read_output_until_empty()
+                    session.destroy()
+                    close_safely(doc)
+                    gc.collect()
+                    uprint("Cancelled.")
+                    hide_progressbar()
+                    return
+
+                image_datas = session.context.read_output_until_empty()
+                if not image_datas:
+                    continue
+                for image_data in image_datas:
+                    image_data: ImageData
+                    # handle successful image data population
+                    if image_data.exception is None:
+                        replace_page(
+                            doc,
+                            page_index=image_data.page_index,
+                            page_bytes=image_data.page_bytes,
+                            page_rect=image_data.page_rect,
+                        )
+                        uprint(f"Page {image_data.page_index + 1} added to the PDF")
+                        finished += 1
+                        update_progress(finished)
+                        continue
+                    # handle exception during populating image data
+                    uprint(
+                        f"Error processing page {image_data.page_index + 1}: {image_data.exception}"
+                    )
+                    processing_error = image_data.exception
+                    # cancel all remaining tasks
+                    session.cancel_all(with_cancel_event_set=True)
+                    break
+
+            if processing_error is not None:
+                # wait for all tasks to exit
+                session.wait_for_all()
+                # empty the output queue
+                _ = session.context.read_output_until_empty()
+                session.destroy()
+                raise processing_error
+            else:
+                # read all remaining output(if any)
+                image_datas = session.context.read_output_until_empty()
+                for image_data in image_datas:
+                    image_data: ImageData
+                    if image_data.exception is not None:
+                        uprint(
+                            f"Error processing page {image_data.page_index + 1}: {image_data.exception}"
+                        )
+                        session.destroy()
+                        raise image_data.exception
+                    replace_page(
+                        doc,
+                        page_index=image_data.page_index,
+                        page_bytes=image_data.page_bytes,
+                        page_rect=image_data.page_rect,
+                    )
+                    finished += 1
+                    update_progress(finished)
+                    uprint(f"Page {image_data.page_index + 1} added to the PDF")
+                session.destroy()
+    except Exception as e:
+        hide_progressbar()
+        close_safely(doc)
+        gc.collect()
+        raise e
+
+    hide_progressbar()
+
+    try:
+        doc.save(output_file)
+    except Exception as e:
+        uprint(f"Error saving PDF file: {e}")
+        raise e
+    else:
+        uprint(f"PDF file saved to {output_file}")
+        uprint(f"Time taken: {(time.time_ns() - start_time) / 1e9:.3f} seconds")
+    finally:
+        close_safely(doc)
+        gc.collect()
 
 
 # ----------------------------------Below are the window and widgets configuration codes--------------------------------
@@ -160,13 +233,15 @@ EXEC_WINDOW_CONFIG = FnExecuteWindowConfig(
     output_dock_initial_size=(None, DEFAULT_OUTPUT_BROWSER_HEIGHT),
 )
 
+WIDGET_CONFIGS = {}
+
 # --------------------------------Add the function to GUIAdapter instance------------------------------------------------
 
 
 def use(adapter: GUIAdapter):
     adapter.add(
         images2pdf,
-        cancelable=False,
+        cancelable=True,
         display_name=FUNC_DISPLAY_NAME,
         # document=FUNC_DOCUMENT,
         # document_format=FUNC_DOCUMENT_FORMAT,
